e856f4016f35d4efd7dd17926aea4c3de0374cb4
leangeder <leangeder@hotmail.com>
2015-09-04 22:31:09 +0000

    second commit

---
+++ services/torrent/bck/rutorrent_user_config.php
@@ -0,0 +1,7 @@
+<?php
+
+$pathToExternals['curl'] = '/usr/bin/curl';
+$topDirectory = '/home/torrent';
+$scgi_port = 5001;
+$scgi_host = '127.0.0.1';
+$XMLRPCMountPoint = '/TORRENT';
---
+++ services/torrent/config/rutorrent_user_config.php
@@ -0,0 +1,8 @@
+<?php
+
+$pathToExternals['curl'] = '/usr/bin/curl';
+$pathToExternals['ffmpeg'] = '/usr/bin/ffmpeg2theora';
+$topDirectory = '/home/torrent';
+$scgi_port = 5000;
+$scgi_host = '127.0.0.1';
+$XMLRPCMountPoint = '/TORRENT';
